a10dff12788ff5492fbcebbd50ba687c9e3af4ea
MikelBarajas38 <mikelbarajas388@gmail.com>
2024-07-08 18:28:14 -0600

    trie practice 2

---
+++ Leetcode/1268-search-suggestion-system.cpp
@@ -0,0 +1,174 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    vector<int> next;
+    bool exists = false;
+
+    Node() {
+        next.assign(26, -1);
+    }
+};
+
+class Trie {
+private:
+    vector<Node> T;
+
+public:
+
+    Trie() {
+        T.emplace_back();
+    }
+
+    void insert(string word) {
+
+        int i = 0;
+        for(char &c : word) {
+            //current = T[i]
+            int index = c - 'a';
+            if(T[i].next[index] == -1) {
+                T[i].next[index] = T.size();
+                T.emplace_back();
+            }
+            i = T[i].next[index];
+        }
+        T[i].exists = true;
+
+    }
+
+    void generateSuggestions(vector<string> &suggestions, string &word, int i, int n) {
+        
+        if(suggestions.size() >= n) {
+            return;
+        }
+
+        if(T[i].exists) {
+            suggestions.push_back(word);
+        }
+
+        for(char c = 'a'; c <= 'z'; c++) {
+            int index = c - 'a';
+            if(T[i].next[index] != -1) {
+                word += c;
+                generateSuggestions(suggestions, word, T[i].next[index], n);
+                word.pop_back();
+            }
+        }
+
+    }
+
+    vector<string> getThreeSuggestions(string prefix) {
+  
+        vector<string> suggestions;
+
+        int i = 0;
+        for(char &c : prefix) {
+            int index = c - 'a';
+            if(T[i].next[index] == -1) {
+                return suggestions;
+            }
+            i = T[i].next[index];
+        }
+
+        generateSuggestions(suggestions, prefix, i, 3);
+
+        return suggestions;
+
+    }
+
+};
+
+
+class Solution {
+public:
+
+    // approach 1: binary search
+    vector<vector<string>> suggestedProducts1(vector<string>& products, string searchWord) {
+        
+        sort(products.begin(), products.end());
+
+        vector<vector<string>> suggestions;
+        string typedWord = "";
+        for(char &c : searchWord) {
+            
+            typedWord += c;
+            vector<string> suggestion;
+
+            auto it = lower_bound(products.begin(), products.end(), typedWord);
+
+            for(int i = 0; i < 3 && it < products.end(); i++) {
+
+                string possibleSuggestion = *it;
+
+                if(!possibleSuggestion.starts_with(typedWord)) {
+                    break;
+                }
+
+                suggestion.push_back(possibleSuggestion);
+                it++;
+
+            }
+
+            suggestions.push_back(suggestion);
+
+        }
+
+        return suggestions;
+
+    }
+
+    //approach 2: use trie
+
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+    
+        // Trie* trie = new Trie(); // heap allocated
+        Trie trie;
+
+        for(string product : products) {
+            trie.insert(product);
+        }
+
+        vector<vector<string>> suggestions;
+        string typedWord = "";
+        for(char &c : searchWord) {
+            
+            typedWord += c;
+            vector<string> suggestion = trie.getThreeSuggestions(typedWord);
+            suggestions.push_back(suggestion);
+
+        }
+
+        return suggestions;
+    
+    }
+
+};
+
+int main() {
+
+    Solution s;
+    vector<string> products = {"mobile","mouse","moneypot","monitor","mousepad"};
+    string searchWord = "mouse";
+    vector<vector<string>> suggestions = s.suggestedProducts(products, searchWord);
+
+    for(vector<string> suggestion : suggestions) {
+        for(string s : suggestion) {
+            cout << s << " ";
+        }
+        cout << endl;
+    }
+
+    vector<string> products2 = {"oqjrtcokaagakchrwrdbrlpnqivwcpzwuxbdkpkhndevouwyrtmbokxolhbvrencthmyplqixnhnokbhtbstmslfbinsypubqjckiqujvmknxuomdwqkfudgiqmpzkvnshrmnoeonzyfaipdcfdwhekrazfkdlfluyvoevahsyhvqjfcizxjhucbpqdjgmqqalqnvfyrtfkyrxlavfbagxkdloetaiedqbrmtzxwwrpyewrcbntsnrdzurzlfokcxolkltouozdobqvtoldjuincumspqpujynxxetbflfnkrscxgrvennmczurjvnoalxcfcrnddfckavedjrvewmawxazviumzpudgolofsxllhgsdrnvjbipcsqsdoosfdkhavhicsfbyavwyxziefmycknhzqujqkbqhacuaebwqpkfnkovxchczohhirczixikuoktkaamchcghynclonujiyzkcghjjjtzckjxmpssfqjirnfvrddvoqvdeteegupzevydcywjsoybsflpgpzkcoztcayffscwxkofwibguysdjtmddgevjhbxrfiqliqyiczdunhosdctrvlagcugpsskpvlsdhcbpdtdwjbhchymlptjrmzmezwljxemgzecptluxbwtgoyvqwrvxkartgrebdegyqdibemnficwpgsywlzczvwujmykkyhkkxhvrtmjxcyidawfxjcagsgutndplixpepddiosflfoujxndtgxsbzarlwacjtydorlrtukbyibaktiphojmrcbwjppnuwwihuerqswjpmyblrdslnwpdnzovpslsrfbzhjywbimwdiqhxmdssjovufgbqqykozfjbylihfwanzjdtgeoimwowocykeskvivwuybdrowolstbrbenioagkgwednhymqisdvjuycjvtrurzqssxrisozysizxeoyelhglnppsjzggjunmyabzptlfktzphlmhvpoqlnrwgxauepumssyecjpwfozhxhvodsasomhtgbasijrpphbijnxcyrajypogighdxacjxqyxagfuenwrdajqsuzrjtrqjebjnbmdzbqdgmbuqofbyegnxvgsegbaqxsdjjffdxiqgqtlgclqtmjqfmbrsccnoidkugbwscamrqfqmpbfuvvoxrodoccusixfehzetsbiflonazsmacvwilarpgpwjkexykqkmklqcuhdrhhgrzofbsjvzwdaghqxajgwqmuhyyervscyiyggbqyhpvotnmaubapigegatgtqeazgcytubsuhjzipbyxnkyqveeetgecxwinwdjuawpzdieizlzqplzajafeernecfkpiilnuqdjnipjybqishinomemdxfwabhemxecuyailbogehxwwtzvaueonxkjmdvojpjjxaftvnbbsfcjjyzwartrbmxgktbhrqeynwoziddoh","kfnyprlqjosvbkvfvanhqsxpfezfafuzxchbwsdagvgtmtwdjsgnodkahyfkpyfmrbrcineichemlomrgaydmjnzbqxhzpciwkwbcsrpeafwkitjibviipnhvcqntjuwnccylnqdwxmirvnggcdwfbldnhxsjjhfqcxlggcpldamlyttzibadxzcpwecjjxekeoucanprljnsgvhpxjydazzlxofypaxcnwhhkhrtwbecouqvoxnzsitdzyrxokhdtynjgygqarnhyaqsdrgdqdmpfwdhlayltvrjalfxxctwgqtmuwgdwzxyuiictaul","kfnwqytfhroyewirkaofdwdjtzyaeamrqkbdzganjfqfykfwizgtxjhrbvnmbwyfivwrbbjralxuptonwhlvztgefnggltqekduxdhznowjsayyaieacjvewhfookivjuwmwmhlobrgummkvjwclbipmkwrpbwqlbthbokenmcaupqbtuglgqtpuognhfachuscdsztjbuffgdzajlzhmnmksdnyrhahnnhilwnqohxyjsiatzshinquevymllusyqzkjwovmgtwwfztpbcpvyjdkqcdkscpimchxdjdrxvropgbgrrccfqnatlqjlojfczeggnanpuqtebjcyezdqpxsfkegxqfeplcmfxyqmlruapeywuftbbosmpegfpcepukcsvpgmfundbvbzazwqnefmbnnxntixtormwfbnuwheahzddwksodogedymeujvxacdbguthxhlcmmblfwjrnurpfuawoghyakqkccxcgxzdraasdivjgvtjrulhgktdxjuzpdyewckrpmjrttusqubhwntpchscprcdddsbyxrqrnplgazwjvojrtxogqdvqarcspvkfljlswpktqxlndidtnhzryohyyujfncunynxkblsdpjlaifdzsjxpuapwjzwzeahrnfkgtaehwjqsligfjnmsivinygzoeaejagsvzkyicbsyfxhqaotexdsvtxuexnmqsjhmojexevbgozlkbmuuexgpklnsetscrceuvtfhrxwvloiucqbzpxwtodopesljmisfxqmxhjlehpyaodfwqbnfvjtplauiyiyrddbjdpkvznmvpbzxgemfhgpwligjwznbztyaenziwooceebfwppfpgmzczoenqtmqcjdgvdlhjpwwlqcyjmaqnjicyrocrvxxrnrhpsqvrndurviilufvwbsswtjlmefydjechqrgeqjucsjyaaudocxgpvtbfekgynmnywltxvqddtjdaunvfqjclrjsohfenjxqjpvcklfdjoaavgoukzqjijycwqipkzpcvaqzqwgwnolrqudrcqqymlhelskpvrlobsjxlgsunkdlvqdgpnnrhcgdxasbdmrcrauswiticdiwkhdvpkduqgqpujcyjkrksmhjswvqccpxfacszfdsdvoxjnlphqawjlxryprgoexjqxryuxadviwdebxjyzolpqerfkxhklgpeffahclnfyfnwudwnjjqbcazsuzekwfsprbiwztqsxtxpiemfucwgcamdowvoymuwkstyqggmtknzezacoklmiaanvsttqknedeoayakomzzzntfmyfaunsnvimvkboqcfprizsofhauflvmetrwfoigjcxjavspnaqwpagfwslxolbjgglknrcacqntfcizfkmcrdbdidvojdritvnvgnsvrhjcutojfvjaspzdodnisyuirklwcxjitakdpxaclthbxgooxmqslftctxopfencxtzktckpkpkhlkboueylshbztlvkbtkpjdcxakldxmnjnqjyscgvydlmnpfnaxaicylivtesvtoqimovynmntiijxnwinlirbmiubpwlmwdscynsywgswsklxaxjwoculminicuphgtepjxmlhaolzzxuqqawjmvlniknniwexrelrvxagbtcqnzhdmiqfkyvwnsfuzskzlcvcyluzjtesuvzdknipreueyqgzvgbeqtmcnzwtfdgmihuzwtsdxahawfiwnpzzwpnzawfzyobyriuxbnlojvkfycwprgngluhgyirhutknuvdyebwrmaarbelkhzoqpilrneitzzuysmlpczdepdngeuwcpylcysafnmzulcrzlkaskbiexjikebbrwlbcpgvmiumsafuzqcuxnpwtjgqmlmnbeauljmyrxykugovjfazsrkzsyonecfejokeiwtxvdjawbkgvlqegxcoxwhldzoblzvhydopqpatwxllccdnlmxjypjiimtczqhubuloozyiwjmcsooqvufuamaopwxsexaaohazzctezrqahkdsnkqspkjqcvocnqirwzxocbxibrwrlrhzcuxlfqsgwoqyagkmcrdedasvrzjgugbrvvbsjtmjfbdxhchbkfomefrfrwdiyhhskrqstnnwrclsphqfrbwjvlitjmwtlmx","kfnyprlqjosvbkvfvanhqsxpfezfafuzxchbwsdagvgtmtwdjsgnodkahyfkpyfmrbrcineichemlomrgaydmjnzbqxhzpciwkwbcsrpeafwkitjibviipnhvcqntjuwnccylnqdwxmirvnggcdwfbldnhxsjjhfqcxlggcpldamlyttzibadxzcpwecjjxekeoucanprljnsgvhpxjydazzlxofypaxcnwhhkhrtwbecouqvoxnzsitdzyuoyzfkyseiohccpdtnjhqlrkgpcifvatradjfurxmwfssmbpbvxeoialjeyxujpgqdunhrthidhizzqddvuqzmoenmjzunulkrjyxfugrpvkwoiwyxwgrweakhbswllbyziranhxkleggegegdailjgyteaghdqnjqdjfhyrapqmckvxgxmasnweej"};
+    string searchWord2 = "kfnyprlqjosvbkvfvanhqsxpfezfafuzxchbwsdagvgtmtwdjsgnodkahyfkpyfmrbrcineichemlomrgaydmjnzbqxhzpciwkwbcsrpeafwkitjibviipnhvcqntjuwnccylnqdwxmirvnggcdwfbldnhxsjjhfqcxlggcpldamlyttzibadxzcpwecjjxekeoucanprljnsgvhpxjydazzlxofypaxcnwhhkhrtwbecouqvoxnzsitdzyrxokhdtynjgygqarnhyaqsdrgdqdmpfwdhlayltvrjalfxxctwgqtmuwgdwzxyuiictaul";
+    
+    vector<vector<string>> suggestions2 = s.suggestedProducts(products2, searchWord2);
+
+    for(vector<string> suggestion : suggestions2) {
+        for(string s : suggestion) {
+            cout << s << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
