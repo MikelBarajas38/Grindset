69db47660b397592a4c252c9edf7ffa8670cd318
MikelBarajas38 <mikelbarajas388@gmail.com>
2024-07-12 15:42:12 -0600

    lost track trying to get a dp

---
+++ Leetcode/1717-maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    int count_and_remove_pattern(string &s, char first, char second) {
+
+        string match = "";
+        int count = 0;
+
+        for(char &c : s) {
+            if(c == second && !match.empty() && match.back() == first) {
+                match.pop_back();
+                count++;
+            } else {
+                match.push_back(c);
+            }
+        }
+
+        s = match;
+
+        return count;
+
+    }
+
+    int maximumGain(string s, int x, int y) {
+        
+        int total_score = 0;
+        if(x > y) {
+            total_score += count_and_remove_pattern(s, 'a', 'b') * x;
+            total_score += count_and_remove_pattern(s, 'b', 'a') * y;
+        } else {
+            total_score += count_and_remove_pattern(s, 'b', 'a') * y;
+            total_score += count_and_remove_pattern(s, 'a', 'b') * x;
+        }
+
+        return total_score;
+
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.maximumGain("ababab", 1, 3) << endl;
+    cout << s.maximumGain("cdbcbbaaabab", 4, 5) << endl;
+    return 0;
+}
