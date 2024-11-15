30f2bea6e852c7ae75839ee3ffe1fc9ed640fce6
MikelBarajas38 <mikelbarajas388@gmail.com>
2024-11-15 08:54:31 -0600

    I truly sold

--- codeforces/2031/C.cpp
+++ codeforces/2031/C.cpp
@@ -13,44 +13,30 @@ void solve() {
     int n;
     cin >> n;
 
-    vector<int> perfect_squares;
-    for(int i = 1; i * i <= n; i++) {
-        perfect_squares.push_back(i * i);
-    }
-    reverse(all(perfect_squares));
-    perfect_squares.pop_back();
-
-    if(perfect_squares.empty()) {
-        cout << "-1\n";
+    if(n % 2 == 0) {
+        int curr = 1;
+        for(int i = 0; i < n; i += 2) {
+            cout << curr << ' ' << curr << ' ';
+            curr++;
+        }
+        cout << '\n';
         return;
     }
 
-    vector<int> ans(n, 0);
-    int curr = 1, used = 0, j = 0;
-
-    for(int i = 0; i < n; i++) {
-
-        if(ans[i] != 0) {
-            continue;
-        }
-
-        if(n - used >= perfect_squares[j]) {
-            ans[i] = curr;
-            ans[i + perfect_squares[j] - 1] = curr;
-            curr++;
-            used += 2;
-        } else if(j < perfect_squares.size()) {
-            j++;
-            i--;
-        } else {
-            cout << "-1\n";
-            return;
-        }
-
+    if (n < 26) {
+        cout << -1 << '\n';
+        return;
     }
 
-    for(int x : ans) {
-        cout << x << ' ';
+    int curr = 2;
+    for(int i = 0; i < n; i++) {
+        if(i == 0 || i == 9 || i == 25) {
+            cout << 1 << ' ';
+        } else {
+            cout << curr << ' ' << curr << ' ';
+            curr++;
+            i++;
+        }
     }
     cout << '\n';
 
